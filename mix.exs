6c9afeb49214c1faa4c16fe5af4a94d3e8d8b812
Ben Wilson <benwilson512@gmail.com>
2015-06-29 15:28:17 -0400

    include other deps and use instance_role

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule ExAwsTest.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger, :ex_aws]]
+    [applications: [:logger, :ex_aws, :poison, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,10 @@ defmodule ExAwsTest.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:ex_aws, "~> 0.3.0"}]
+    [
+      ex_aws: "~> 0.3.0",
+      poison:    "~> 1.2.0",
+      httpoison: "~> 0.6.0"
+    ]
   end
 end
